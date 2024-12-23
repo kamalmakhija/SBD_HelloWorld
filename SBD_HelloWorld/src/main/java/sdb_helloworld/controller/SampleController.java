8d1219dc75300808e9a3d59f8cad6742c952918e
Kamal <Kamal@DESKTOP-MK2Q7H6>
2024-12-23 22:18:35 +0530

    Commit 3 : Added Sys Logs statements.

--- SBD_HelloWorld/src/main/java/sdb_helloworld/controller/SampleController.java
+++ SBD_HelloWorld/src/main/java/sdb_helloworld/controller/SampleController.java
@@ -8,6 +8,7 @@ public class SampleController {
 	
 	@GetMapping(value="/hello")
 	public String helloWorld() {
+		System.out.println("Sending Logs to AWS CloudWatch From Application System.out");
 		return "HelloWorld From SBD_HelloWorld Application";
 	}
 	
